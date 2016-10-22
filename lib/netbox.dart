7b746ae1074697c23ea8911ac516bc4f1bf20ffc
kyorohiro <kyorohiro@gmail.com>
2016-10-22 11:50:50 +0900

    ..

--- lib/netbox.dart
+++ lib/netbox.dart
@@ -6,7 +6,7 @@ import 'package:firefirestyle.httprequest/request.dart' as req;
 import 'package:firefirestyle.httprequest/request_ver_html.dart' as req;
 import 'package:firefirestyle.miniprop/miniprop.dart' as prop;
 //import 'dart:convert' as conv;
-//import 'dart:typed_data' as typed;
+import 'dart:typed_data' as typed;
 //
 //
 //
@@ -21,6 +21,45 @@ class UploadFileProp {
   UploadFileProp(this.prop) {}
 
   String get blobKey => prop.getString("blobkey", "");
+}
+
+class FileNBox {
+  req.NetBuilder builder;
+  String backAddr;
+  String callbackopt = "cb";
+  FileNBox(this.builder, this.backAddr) {}
+
+  Future<String> getFromKey(String key) async {
+    return "${backAddr}/api/v1/blob/get?key=${Uri.encodeComponent(key)}";
+  }
+
+  Future<UploadFileProp> updateIcon(String accessToken, String dir, String name, typed.Uint8List data) async {
+    String url = [
+      backAddr, //
+      """/api/v1/blob/requesturl""", //
+      """?dir=${Uri.encodeComponent(dir)}&file=${Uri.encodeComponent(name)}"""
+    ].join("");
+
+    var uelPropObj = new prop.MiniProp();
+    uelPropObj.setString("token", accessToken);
+    req.Response response = await (await builder.createRequester()).request(req.Requester.TYPE_POST, url, data: uelPropObj.toJson(errorIsThrow: false));
+    if (response.status != 200) {
+      throw "failed to get request token";
+    }
+    var responsePropObj = new prop.MiniProp.fromByte(response.response.asUint8List());
+    var tokenUrl = responsePropObj.getString("token", "");
+    //new prop.MiniProp.fromByte(response.response.asUint8List());
+    print(""" TokenUrl = ${tokenUrl} """);
+    req.Multipart multipartObj = new req.Multipart();
+    var responseFromUploaded = await multipartObj.post(await builder.createRequester(), tokenUrl, [
+      new req.MultipartItem.fromList("file", "blob", "image/png", data) //
+    ]);
+    if (responseFromUploaded.status != 200) {
+      throw "failed to uploaded";
+    }
+
+    return new UploadFileProp(new prop.MiniProp.fromByte(responseFromUploaded.response.asUint8List(), errorIsThrow: false));
+  }
 }
 
 class MeNBox {
@@ -45,32 +84,8 @@ class MeNBox {
     return new LogoutProp(new prop.MiniProp.fromByte(response.response.asUint8List(), errorIsThrow: false));
   }
 
-  Future<UploadFileProp> updateIcon(String src, String userName, String accessToken) async {
-    String url = [
-      backAddr, //
-      """/api/v1/blob/requesturl""", //
-      """?dir=${Uri.encodeComponent("/user/"+userName)}&file=meicon"""
-    ].join("");
-
-    var uelPropObj = new prop.MiniProp();
-    uelPropObj.setString("token", accessToken);
-    req.Response response = await (await builder.createRequester()).request(req.Requester.TYPE_POST, url, data: uelPropObj.toJson(errorIsThrow: false));
-    if (response.status != 200) {
-      throw "failed to get request token";
-    }
-    var responsePropObj = new prop.MiniProp.fromByte(response.response.asUint8List());
-    var tokenUrl = responsePropObj.getString("token", "");
-    //new prop.MiniProp.fromByte(response.response.asUint8List());
-    print(""" TokenUrl = ${tokenUrl} """);
-    req.Multipart multipartObj = new req.Multipart();
-    var responseFromUploaded = await multipartObj.post(await builder.createRequester(), tokenUrl, [
-      new req.MultipartItem.fromBase64("file", "blob", "image/png", src.replaceFirst(new RegExp(".*,"), '')) //
-    ]);
-    if (responseFromUploaded.status != 200) {
-      throw "failed to uploaded";
-    }
-
-    return new UploadFileProp(new prop.MiniProp.fromByte(response.response.asUint8List(), errorIsThrow: false));
+  Future<UploadFileProp> updateIcon(String accessToken, String userName, typed.Uint8List data) async {
+    return (new FileNBox(builder, backAddr)).updateIcon(accessToken, "/user/" + userName, "meicon", data);
   }
 }
 

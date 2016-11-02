e39a213d7e735359ec7fb2df3f868a79e93b0347
kyorohiro <kyorohiro@gmail.com>
2016-11-02 15:51:45 +0900

    ..

--- lib/src/usernbox.dart
+++ lib/src/usernbox.dart
@@ -28,17 +28,29 @@ class UserInfoProp {
   String get content => prop.getString("Cont", "");
 }
 
-
 class UserNBox {
   static const String modeNewOwder = "";
   static const String modeMPoint = "-point";
   req.NetBuilder builder;
   String backAddr;
   UserNBox(this.builder, this.backAddr) {}
-  Future<String> makeUserBlobUrlFromKey(String key) async {
-    key = key.replaceAll("key://", "");
-    return "${backAddr}/api/v1/user/getblob?key=${Uri.encodeComponent(key)}";
+  Future<String> makeUserBlobFromKey(String key) async {
+    return makeUserBlob(key);
   }
+  Future<String> makeUserBlobPath(String useName, String dir, String file, {String sign: ""}) async {
+    return makeUserBlob("",useName: useName,dir:dir,file: file,sign: sign);
+  }
+  Future<String> makeUserBlob(String key, {String useName: "", String dir: "", String file: "", String sign: ""}) async {
+    key = key.replaceAll("key://", "");
+    return [
+      """${backAddr}/api/v1/user/getblob""", //
+      """?key=${Uri.encodeComponent(key)}""", //
+      """&dir=${Uri.encodeComponent(dir)}""",
+      """&file=${Uri.encodeComponent(file)}""",
+      """&sign=${Uri.encodeComponent(sign)}""",
+    ].join("");
+  }
+
   //
   Future<UserInfoProp> getUserInfo(String userName) async {
     var requester = await builder.createRequester();
@@ -60,7 +72,7 @@ class UserNBox {
     return new UserInfoProp(new prop.MiniProp.fromByte(response.response.asUint8List(), errorIsThrow: false));
   }
 
-  Future<UserKeyListProp> findUser(String cursor,{String group:"",String mode:""}) async {
+  Future<UserKeyListProp> findUser(String cursor, {String group: "", String mode: ""}) async {
     var url = "${backAddr}/api/v1/user/find?mode=${Uri.encodeComponent(mode)}&group=${Uri.encodeComponent(group)}";
     var requester = await builder.createRequester();
     req.Response response = await requester.request(req.Requester.TYPE_GET, url);

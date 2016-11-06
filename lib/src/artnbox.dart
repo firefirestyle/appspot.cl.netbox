b5a234f0b653de5ebbde221b1c0e2ab357e724ba
kyorohiro <kyorohiro@gmail.com>
2016-11-06 16:03:43 +0900

    add target property in article finder

--- lib/src/artnbox.dart
+++ lib/src/artnbox.dart
@@ -117,8 +117,8 @@ class ArtNBox {
     return new NewArtProp(new prop.MiniProp.fromByte(response.response.asUint8List(), errorIsThrow: false));
   }
 
-  Future<ArtKeyListProp> findArticle(String cursor,{String userName:""}) async {
-    var url = "${backAddr}/api/v1/art/find?userName=${Uri.encodeComponent(userName)}";
+  Future<ArtKeyListProp> findArticle(String cursor,{String userName:"",String  target:""}) async {
+    var url = "${backAddr}/api/v1/art/find?userName=${Uri.encodeComponent(userName)}&?target=${Uri.encodeComponent(target)}";
     var requester = await builder.createRequester();
     req.Response response = await requester.request(req.Requester.TYPE_GET, url);
     if (response.status != 200) {
